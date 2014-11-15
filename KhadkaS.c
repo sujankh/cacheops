bcf3f6e30dabbfe640035dbf5f8c87be08f3a4df
Sujan <sujank@uab.edu>
2014-11-15 16:14:42 -0600

    Added functionality to operate on block size

--- KhadkaS.c
+++ KhadkaS.c
@@ -1,34 +1,71 @@
 #include <sys/time.h>
 #include <stdio.h>
-#define dataSize 64 * 1024 * 1024
-#define minSize 4 * 1024
+
+#define KB 1024
+#define MB KB * KB
+#define dataSize 64 * MB
+#define minSize 4 * KB
 
 char x[dataSize];
+
+struct timeval startTime, endTime;
+int i, repeat;
+void (*printer)(int, double, int);
 
 double getTimeDifference(struct timeval* start, struct timeval* end)
 {
  return  (end->tv_sec + end->tv_usec / 1000000.0) - (start->tv_sec + start->tv_usec / 1000000.0);
 }
 
+void printCacheSize(int sizeMoved, double time, int bytesJumped)
+{
+printf("%d\t\t%lf\n", sizeMoved, time);
+}
+
+void printBlockSize(int sizeMoved, double time, int bytesJumped)
+{
+printf("%d\t\t%lf\n", bytesJumped, time);
+}
+
+void loop(int numOfLoop, int numBytesToMove, int bytesToJump)
+{
+    gettimeofday(&startTime, NULL);      
+
+    for(repeat = 1; repeat <= numOfLoop; repeat++)
+      {
+       for(i = 0; i < numBytesToMove; i += bytesToJump)
+       {	  
+	  x[i]++;	 
+	}
+      }
+
+      gettimeofday(&endTime, NULL);
+      
+//printf(" %d\t\t%lf\n", numBytesToMove, getTimeDifference(&startTime, &endTime));
+printer(numBytesToMove, getTimeDifference(&startTime, &endTime), bytesToJump);
+}
+
 int main()
 { 
-  struct timeval totalExecStart, totalExecEnd, startTime, endTime;
+  struct timeval totalExecStart, totalExecEnd;
 
   gettimeofday(&totalExecStart, NULL);
 
-  int i,j,  repeat, power2 ;
+  int i, j;
   
-  int numBytesToMove, numOfLoop;
+int numBytesToMove, blockSize,  numOfLoop;
   
   double timeforFiftyLoops;
   
   int loopFactor;
+/*
+printf("Cache size");
 
   gettimeofday(&startTime, NULL);
   
-  for(j = 1; j <= 50; j++)
+   for(j = 1; j <= 50; j++)
     {
-      for(i = 0; i < dataSize; i += 32)
+      for(i = 0; i < dataSize; i += 64)
 	{
 	  x[i]++;
 	}
@@ -43,30 +80,39 @@ int main()
   printf("%d\n", loopFactor);
   
   numBytesToMove = dataSize;
+  
+printer = printCacheSize;
+
   while(numBytesToMove >= minSize)
   {
       numOfLoop = (dataSize/ numBytesToMove) * loopFactor;
        
-      gettimeofday(&startTime, NULL);      
+loop(numOfLoop, numBytesToMove, 64);
 
-    for(repeat = 1; repeat <= numOfLoop; repeat++)
-      {
-       for(i = 0; i < numBytesToMove; i += 32)
-       {	  
-	  x[i]++;	 
-	}
-      }
-
-      gettimeofday(&endTime, NULL);
-      
-      printf(" %d\t\t%lf\n", numBytesToMove, getTimeDifference(&startTime, &endTime));
-      
-      if(numBytesToMove > 8 * 1024 * 1024 || numBytesToMove <= 1024 * 1024)
+      if(numBytesToMove > 8 * MB || numBytesToMove <= 1 * MB)
 	numBytesToMove /= 2;
       else
-	numBytesToMove -= 1024 * 1024;
-    } 
-  gettimeofday(&totalExecEnd, NULL);
+	numBytesToMove -= MB;
+   } 
+
+//loop(1, 64 * MB, 64);
+*/
+printf("%s\n", "Block Size");
+
+printer = printBlockSize;
+
+  //BLOCK SIZE
+numBytesToMove = 64 * KB; //L3 cache for now
+  blockSize = 256;
+   
+  while(blockSize >= 4)
+  {
+numOfLoop = blockSize * 800;
+   loop(numOfLoop, numBytesToMove, blockSize);
+   blockSize /= 2;
+  }
+
+gettimeofday(&totalExecEnd, NULL);
   
   printf("Total exec time: %lf\n", getTimeDifference(&totalExecStart, &totalExecEnd));
 }
