f1f8daec1043a688c8dac8a004f3360cbd342507
Sujan <sujank@uab.edu>
2014-11-18 23:44:21 -0600

    Block size

--- KhadkaS.c
+++ KhadkaS.c
@@ -7,10 +7,9 @@
 #define minSize 4 * KB
 
 char x[dataSize];
-
 struct timeval startTime, endTime;
-int i, repeat;
-int cacheSize[4];
+int  numOfLoop, loopFactor, repeat, cacheSize[4];
+double numBytesToMove, timeTaken, prevTime, difference;
 
 double getTimeDifference(struct timeval* start, struct timeval* end)
 {
@@ -24,6 +23,7 @@ printf("%d\t\t%lf", size, time);
 
 double loop(int numOfLoop, int numBytesToMove, int bytesToJump)
 {
+  int i;
     gettimeofday(&startTime, NULL);      
 
     for(repeat = 1; repeat <= numOfLoop; repeat++)
@@ -42,26 +42,23 @@ return  getTimeDifference(&startTime, &endTime);
 void calculateCacheSize()
 {
   double times[20], sizes[20];
-  double numBytesToMove, sizeFactor, k, timeTaken, prevTime, difference,
-    notedPercentDrop, timeforFiftyLoops;
-  int  numOfLoop, threshold, loopFactor, i, j, nextCacheLikely = 1;
-
+  double  sizeFactor, k, notedPercentDrop, timeforFiftyLoops;
+  int   threshold, nextCacheLikely = 1;
+  int i, j;
 printf("Cache size\n");  
 timeforFiftyLoops = loop(50, dataSize, 64);
  loopFactor = (int) 1000 / timeforFiftyLoops; //adjust so that each jump will take around 20 seconds
   
- sizeFactor = 3.0/4; k = 8.0/9; i = 0;
-numBytesToMove = dataSize;
+ sizeFactor = 3.0/4; k = 8.0/9;
+ i = 0;
+ numBytesToMove = dataSize;
 
   while(numBytesToMove >= minSize)
   {
       numOfLoop = (dataSize/ numBytesToMove) * loopFactor;
-     
       timeTaken = loop(numOfLoop, (int)numBytesToMove, 64);
-
       times[i] = timeTaken;
-      sizes[i] = numBytesToMove;
-	i++;
+      sizes[i] = numBytesToMove; i++;
       
       if(numBytesToMove > 2 * MB)
 	{
@@ -107,52 +104,60 @@ numBytesToMove = dataSize;
 
 }
 
-/*
+
 void calculateBlockSize()
 {
-  int i;   
- printf("%s\n", "Block Size");
+  int blockSize, i, j;
 
-  //BLOCK SIZE
-cacheSize[0] = 32 * KB;
-cacheSize[1] = 2048 * KB;
-cacheSize[2] = 3 * MB;
-cacheSize[3] = 0;
-
-//numBytesToMove = 64 * KB; //L3 cache for now
-
-for(i = 0; i < 2; i++)
+  printf("%s\n", "Block Size");
+  
+  /* for(i = 0; cacheSize[i] != 0; i++)
   {
 printf("L%d Cache\n", i + 1);
 numBytesToMove = cacheSize[i];
-
  blockSize = 256;
 timeTaken = 0, prevTime = 0;   
 loopFactor = (10000 / (i + 1)) / ((i + 1) * (i + 1) / (i + 1)) *  1000.0 * KB / numBytesToMove;
 printf("%d\n", loopFactor);
-
   while(blockSize >= 4)
   {
    numOfLoop = blockSize * loopFactor;
-
    prevTime = timeTaken;
    timeTaken = loop(numOfLoop, numBytesToMove, blockSize);
    
    printSizeTime(blockSize, timeTaken); printf("\t%lf\n", prevTime / timeTaken);
-
    blockSize /= 2;
   }
-  }
+  }*/
+
+ blockSize = 256;
+timeTaken = 0, prevTime = 0;   
+ numOfLoop = 4000;
+
+  while(blockSize >= 4)
+    {
+      prevTime = timeTaken;
+      timeTaken = loop(numOfLoop, dataSize, blockSize);
+   
+      printSizeTime(blockSize, timeTaken); printf("\t%lf\n", prevTime / timeTaken);
+      blockSize /= 2;
+      numOfLoop /= 2;
+    }
 }
-*/
+
 int main()
 { 
   struct timeval totalExecStart, totalExecEnd;
-  gettimeofday(&totalExecStart, NULL);
   int i, j;
+  gettimeofday(&totalExecStart, NULL);
 
-  calculateCacheSize();
+  //calculateCacheSize();
+  cacheSize[0] = 32 * KB; cacheSize[1] = 256 * KB; cacheSize[2] = 3 * MB;
+  calculateBlockSize();
 
+  //  for(i = 3; i >= 0; i--)
+  // printf("%d\n", cacheSize[i]);
+ 
 /*numBytesToMove = 64 * MB;
 numOfBlocksInCache = 3 * MB / 64;
 
